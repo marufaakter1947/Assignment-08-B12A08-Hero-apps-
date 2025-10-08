d4d981002e5dc02a1e5ac097d9bf0689794c31f6
Marufa Akter <marufaakter1947@gmail.com>
2025-10-08 13:16:02 +0600

    Load all data in Apps page and implement search functionality

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useApps from '../Hooks/useApps';
+import Banner from '../Components/Banner';
+import AppCard from '../Components/AppCard';
 
 const Apps = () => {
+     const {apps} =useApps()
+     const [search,setSearch] = useState("")
+    // console.log(search)
+    const term = search.trim().toLocaleLowerCase()
+   const searchedApps = term ? apps.filter(app => app.title.toLocaleLowerCase().includes(term)) : apps
+   
     return (
         <div>
-            <h1>Apps page</h1>
+            <h1 className='text-[48px] font-bold text-center mb-4 '>Our All Applications</h1>
+            <p className='text-xl text-[#627382] text-center mb-10'>Explore All Apps on the Market developed by us. We code for Millions</p>
+            <div className='flex justify-between mb-[22px] items-center'>
+                <p className='text-xl md:text-2xl font-semibold '>({searchedApps.length})Apps Found</p>
+                <div>
+                    <label className="input">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <circle cx="11" cy="11" r="8"></circle>
+      <path d="m21 21-4.3-4.3"></path>
+    </g>
+  </svg>
+  <input value={search} onChange={(e)=>setSearch(e.target.value)} type="search" required placeholder="Search Apps" />
+</label>
+                </div>
+            </div>
+           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+             {
+               searchedApps.map(app =>(
+                    <AppCard key={app.id} app={app}></AppCard>
+                ))
+            }
+           </div>
         </div>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
 import AppCard from '../Components/AppCard';
 import Banner from '../Components/Banner';
 import BannerCard from '../Components/BannerCard';
+import useApps from '../Hooks/useApps';
+import { NavLink } from 'react-router';
 
 const Home = () => {
-     const appsData = useLoaderData();
-    //  console.log(Apps);
-    const trendingApps = appsData.slice(0,8)
+     
+    const {apps,loading,error} = useApps();
+    
+    const trendingApps = apps.slice(0,8)
     return (
         <div>
             <div>
@@ -24,6 +26,10 @@ const Home = () => {
                     <AppCard key={app.id} app={app}></AppCard>
                 ))
             }
+           </div>
+
+           <div className='flex justify-center items-center'>
+            <button className='btn text-[16px] font-semibold bg-[linear-gradient(125.07deg,_#632EE3,_#9F62F2)] text-white mt-10  '><NavLink to="/apps">Show All</NavLink></button>
            </div>
         </div>
     );
